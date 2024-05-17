7749038cb2150e05b97ffc6dca34a732bc7392f4
jemojeremmy2@gmail.com <jemojeremmy2>
2024-05-18 01:51:54 +0300

    2

---
+++ lib/app.py
@@ -0,0 +1,9 @@
+print("Hello world!", end=" ")
+print ("hi jeremiah i love so mach how are you doing, ",end="!")
+print("Hello world! am coming through",end="!")
+print("Hello sun!",end="!")
+print("Hello sky!",end="!")
+# lib/app.py
+
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
